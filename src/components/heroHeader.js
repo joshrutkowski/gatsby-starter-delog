9a4847119228f84faa21176ebbffd0827812ff4b
Josh <jr.rtkski@gmail.com>
2021-01-15 12:49:47 -0700

    Added social icons

--- src/components/heroHeader.js
+++ src/components/heroHeader.js
@@ -1,5 +1,7 @@
 import React from "react"
-import { StaticQuery, graphql, Link } from "gatsby"
+import { StaticQuery, graphql} from "gatsby"
+import { SocialIcon } from 'react-social-icons';
+
 export default () => (
   <StaticQuery
     query={graphql`
@@ -23,6 +25,10 @@ export default () => (
         />
         <div className="primary-content" >I can help you solve a problem or launch a new product. 🚀 </div>
         <a href="mailto:hello@joshrutkowski.com" className="button -primary">Get in touch &rarr;</a>
+        <SocialIcon url="http://twitter.com/joshuarutkowski" bgColor="#2E3441" fgColor="#fff" style={{ marginLeft: '.5rem', height: 35, width: 35 }}/>
+        <SocialIcon url="http://linkedin.com/in/joshuarutkowski" bgColor="#2E3441" fgColor="#fff"style={{ marginLeft: '.5rem', height: 35, width: 35 }} />
+        <SocialIcon url="http://github.com/joshrutkowski" bgColor="#2E3441" fgColor="#fff"style={{ marginLeft: '.5rem', height: 35, width: 35 }}/>
+
       </div>
     )}
   />
